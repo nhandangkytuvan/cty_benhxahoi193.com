787e089bac70e2b26cc847f0575eb8abfb0f7f50
Pham Van Truong <nhandangkytuvan@gmail.com>
2017-06-01 14:54:51 +0700

    oke

---
+++ resources/views/layout/mobile.blade.php
@@ -0,0 +1,171 @@
+<!DOCTYPE html>
+<html lang="vi">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="language" content="vi" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="classification" content="hospital" />
+	<meta name="distribution" content="Global" />
+	<meta name="rating" content="General" />
+	<meta name="robots" content="index, follow" />
+	<meta name="creator" content="Phòng khám Nam Khang" />
+	<meta name="publisher" content="Phòng khám Nam Khang" />
+    <meta name="author" content="{{ $setting->web_name }}">
+    <!-- csrf -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <!-- facebook -->
+    <meta property="og:site_name" content="Phòng Khám Nam Khang" />
+	<meta property="og:type" content="article" />
+    <meta property="og:locale " content="vi_VN">
+    <!-- link rss,sitemap -->
+    
+    <!-- link icon -->
+    <link rel="shortcut icon" href="{{ asset('public/img/'.$setting->web_icon) }}" type="image/x-icon">
+    @yield('title')
+    @yield('keyword')
+    <link rel="stylesheet" type="text/css" href="{{ asset('public/css/global/glyphicon-bootstrap/glyphicon-bootstrap.css') }}">
+    <link rel="stylesheet" type="text/css" href="{{ asset('public/css/global/font-awesome/css/font-awesome.css') }}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('public/css/global/slick/slick.css') }}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('public/css/global/slick/slick-theme.css') }}">
+
+	<link rel="stylesheet" type="text/css" href="{{ asset('public/css/mobile/mobile.css') }}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('public/css/mobile/mobile-header.css') }}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('public/css/mobile/mobile-footer.css') }}">
+	@yield('css')
+	<link rel="stylesheet" href="{{ asset('public/css/mobile/mobile-mediaqueries.css') }}">
+	<script type="text/javascript" src="{{ asset('public/js/global/jquery-1.12.3.min.js') }}"></script>
+	<script type="text/javascript" src="{{ asset('public/js/global/jquery.popupoverlay.js') }}"></script>
+	<script type="text/javascript" src="{{ asset('public/js/global/jquery-scrolltofixed-min.js') }}"></script>
+	<script type="text/javascript" src="{{ asset('public/js/global/slick/slick.js') }}"></script>
+	<script type="text/javascript" src="{{ asset('public/js/mobile/shake.js') }}"></script>
+	<script type="text/javascript" src="{{ asset('public/js/mobile/mobile.js') }}"></script>
+	@yield('js')
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-85212676-12', 'auto');
+	  ga('send', 'pageview');
+
+</script>
+</head>
+<body>
+	<header>
+		<div class="container">
+			<div class="dis-table width-100">
+				<div class="table-cell">
+					<div>
+						<a href="{{ url('/') }}" title="phòng khám chuyên khoa nam khang">
+							@if(url()->current()==url('chuyen-de/sui-mao-ga'))
+								<img src="{{ asset('public/images/chuyende/mobile/suimaoga/suimaoga-2.png') }}" class="center-block">
+							@else
+								<img src="{{ asset('public/images/mobile2/logo.png') }}" class="center-block">
+							@endif
+						</a>
+					</div>
+				</div>
+				<div class="table-cell">
+					@if(Request::url()==url('/'))
+					<h1 class="text-uppercase">phòng khám chuyên khoa nam khang</h1>
+					@else
+					<div class="h3 text-uppercase">phòng khám chuyên khoa nam khang</div>
+					@endif
+					<div class="web-address text-uppercase">số 193C1 bà triệu - hai bà trưng - hà nội</div>
+				</div>
+				<div class="table-cell">
+					@if(url()->current()==url('chuyen-de/sui-mao-ga'))
+						<img src="{{ asset('public/images/chuyende/mobile/suimaoga/suimaoga-3.png') }}" alt="doc">
+					@else
+						<img src="{{ asset('public/images/mobile/header-1.png') }}" alt="doc">
+					@endif
+				</div>
+				<div class="table-cell">
+					<span class="text-uppercase">Chuyên khoa<br>bệnh xã hội</span>
+				</div>
+			</div>
+		</div>
+	</header>
+	<menu class="container">
+		<div class="dis-table width-100">
+			<a class="table-cell text-center text-uppercase" href="{{ url('/') }}" title="Trang chủ">
+				<span>Trang chủ</span>
+			</a>
+			<a class="table-cell text-center text-uppercase" href="{{ url('ve-chung-toi') }}" title="Giới thiệu phòng khám">
+				Giới thiệu<br>phòng khám
+			</a>
+			<a class="table-cell text-center text-uppercase" href="#" title="Đội ngũ chuyên gia">
+				Đội ngũ<br>chuyên gia
+			</a>
+			<a class="table-cell text-center text-uppercase" href="{{ url('dia-chi-phong-kham') }}" title="Địa chỉ phòng khám">
+				Địa chỉ<br>phòng khám
+			</a>
+		</div>
+	</menu>
+	<div class="banner container">
+		<div class="slick-home">
+			<div>
+				<a href="http://swt.phongkham193.com/LR/Chatpre.aspx?id=MFI63108226&cid=1489654963660812714370&lng=en">
+					<img src="{{ asset('public/images/mobile2/slide-1.png') }}" alt="Phòng khám Nam Khang" class="center-block img-responsive">
+				</a>
+			</div>
+			<div>
+				<a href="http://swt.phongkham193.com/LR/Chatpre.aspx?id=MFI63108226&cid=1489654963660812714370&lng=en">
+					<img src="{{ asset('public/images/mobile/slide-2.png') }}" alt="Phòng khám Nam Khang" class="center-block img-responsive">
+				</a>
+			</div>
+		</div>
+		<script>
+			$('.slick-home').slick({
+				autoplay: true,
+				autoplaySpeed: 4000,
+			});
+		</script>
+	</div>
+	@yield('content')	
+	<footer>
+		<div class="row1 container">
+			<div class="dis-table table1 width-100">
+				<div class="table-cell">
+					<div class="text-center">
+						<a href="{{ url('/') }}" title="PHÒNG KHÁM CHUYÊN KHOA NAM KHANG"><img src="{{ asset('public/images/mobile2/logo.png') }}" alt="logo"></a>
+					</div>
+					<div class="text-uppercase text-center">
+						PHÒNG KHÁM CHUYÊN KHOA NAM KHANG
+					</div>
+					<div class="text-uppercase text-center">
+						____PHÒNG KHÁM UY TÍN Ở HÀ NỘI____
+					</div>
+				</div>
+				<div class="table-cell">
+					<div class="text-uppercase">LIÊN HỆ VỚI CHÚNG TÔI</div>
+					<div class="dis-table table2 width-100">
+						<div class="table-cell">
+							<img src="{{ asset('public/images/mobile/footer-1.png') }}" alt="phone" class="">
+						</div>
+						<div class="table-cell">
+							<div><a href="tel:18006181" class="width-100" title="ĐIỆN THOẠI TƯ VẤN MIỄN PHÍ">ĐIỆN THOẠI<br>TƯ VẤN MIỄN PHÍ</a></div>
+							<div><a href="tel:18006181" class="width-100" title="ĐIỆN THOẠI TƯ VẤN MIỄN PHÍ">1800-6181</a></div>
+						</div>
+					</div>
+					<div>
+						<a href="{{ url('dia-chi-phong-kham') }}" title="Số 193c1 Bà Triệu Hai Bà Trưng, Hà Nội">
+							Số 193c1 Bà Triệu<br>Hai Bà Trưng, Hà Nội
+						</a>
+					</div>
+				</div>
+			</div>
+		</div>
+		@yield('toolbar')	
+	</footer>
+	<script language="javascript" src="http://swt.phongkham193.com/JS/LsJS.aspx?siteid=MFI63108226&float=1&lng=en"></script>
+	<div id="my_popup">
+	    <p>Bác sỹ đang yêu cầu được chát với bạn</p>
+	    <p class="dis-none">Sẽ nhanh hơn khi bạn trò chuyện với bác sĩ tư vấn</p>
+	    <a class="my_popup_close btn-cancel">Từ chối</a>
+	    <a href="http://swt.phongkham193.com/LR/Chatpre.aspx?id=MFI63108226&cid=1489654963660812714370&lng=en" class="btn-agree">Đồng ý</a>
+	</div>
+</body>
+</html>
---
+++ resources/views/web/mobile/home.blade.php
@@ -0,0 +1,252 @@
+@extends('layout.mobile')
+@section('title')
+    <title>{{ $setting->web_name }}</title>
+@endsection('title')
+@section('keyword')
+	@include('seo.seo_home')
+@endsection('keyword')
+@section('css')
+	<link rel="canonical" href="{{ url('/') }}" />
+    <link rel="stylesheet" href="{{ asset('public/css/mobile/mobile-home.css') }}">
+@endsection('css')
+@section('content')
+	<main class="container home">
+		<div class="row1">
+			<div class="text-center text-uppercase line1">Hỏi bác sỹ nhanh,  chuyên gia online 24/7</div>
+			<div class="dis-table width-100 table1">
+				<div class="table-cell">
+					<a href="http://swt.phongkham193.com/LR/Chatpre.aspx?id=MFI63108226&cid=1489654963660812714370&lng=en" title="Tư vấn trực tuyến">
+						<img src="{{ asset('public/images/mobile/home-1.png') }}" alt="Tư vấn trực tuyến" class="img-responsive">
+					</a>
+				</div>
+				<div class="table-cell">
+					<a href="tel:18006181" title="1800 6181">
+						<img src="{{ asset('public/images/mobile/home-2.png') }}" alt="1800 6181" class="img-responsive">
+					</a>
+				</div>
+			</div>
+		</div>
+		<div class="row2 global1">
+			<div class="dis-table width-100">
+				<div class="text-uppercase table-cell">ĐỘi ngũ chuyên gia Nam Khang</div>
+				<div class="table-cell">
+					<a href="http://swt.phongkham193.com/LR/Chatpre.aspx?id=MFI63108226&cid=1489654963660812714370&lng=en" class="text-center arrow_box dis-table" title="Tìm hiểu thêm"><span class="table-cell text-center">Tìm hiểu thêm</span></a>
+				</div>
+			</div>
+		</div>
+		<div class="row3">
+			<a href="http://swt.phongkham193.com/LR/Chatpre.aspx?id=MFI63108226&cid=1489654963660812714370&lng=en">
+				<img src="{{ asset('public/images/mobile/home-3.png') }}" alt="ĐỘi ngũ chuyên gia Nam Khang" class="img-responsive">
+			</a>
+		</div>
+		<div class="row4">
+			<div class="dis-table">
+				<a class="table-cell text-center" href="http://swt.phongkham193.com/LR/Chatpre.aspx?id=MFI63108226&cid=1489654963660812714370&lng=en">
+					Tư vấn<br>đặt hẹn
+				</a>
+				<div class="table-cell">
+					<div>
+						Lựa chọn đúng bác sĩ là đã thành công một nửa. Mỗi chuyên gia Phòng khám chuyên khoa Nam Khang đều có hơn 20 năm kinh nghiệm khám và điều trị. Chẩn đoán chính xác, hiệu quả rõ rệt, chăm sóc tận tình là “thương hiệu” của họ.
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="row5 global1">
+			<div class="dis-table width-100">
+				<div class="text-uppercase table-cell">Danh mục bệnh</div>
+				<div class="table-cell">
+					<a href="http://swt.phongkham193.com/LR/Chatpre.aspx?id=MFI63108226&cid=1489654963660812714370&lng=en" class="text-center arrow_box dis-table" title="Tìm hiểu thêm"><span class="table-cell text-center">Tìm hiểu thêm</span></a>
+				</div>
+			</div>
+		</div>
+		<div class="row6">
+			<div class="dis-table width-100 table1">
+				<div class="table-cell text-center">
+					<div><h2 class="text-uppercase"><a href="{{ MyAPI::getUrlTermID(28) }}">Sùi mào gà</a></h2></div>
+				</div>
+				<div class="table-cell">
+					<a href="" class="text-center dis-table width-100" title="Nguyên nhân"><span class="table-cell">Nguyên nhân</span></a>
+					<a href="" class="text-center dis-table width-100" title="Triệu chứng"><span class="table-cell">Triệu chứng</span></a>
+				</div>
+				<div class="table-cell">
+					<a href="" class="text-center dis-table width-100" title="Nguy hại"><span class="table-cell">Nguy hại</span></a>
+					<a href="" class="text-center dis-table width-100" title="Điều trị"><span class="table-cell">Điều trị</span></a>
+				</div>
+				<div class="table-cell">
+					<a href="" class="text-center dis-table width-100" title="Chi phí"><span class="table-cell">Chi phí</span></a>
+					<a href="" class="text-center dis-table width-100" title="Kiểm tra"><span class="table-cell">Kiểm tra</span></a>
+				</div>
+			</div>
+			<div class="dis-table width-100 table1">
+				<div class="table-cell text-center">
+					<div><h2 class="text-uppercase"><a href="{{ MyAPI::getUrlTermID(30) }}">Bệnh lậu</a></h2></div>
+				</div>
+				<div class="table-cell">
+					<a href="" class="text-center dis-table width-100" title="Nguyên nhân"><span class="table-cell">Nguyên nhân</span></a>
+					<a href="" class="text-center dis-table width-100" title="Triệu chứng"><span class="table-cell">Triệu chứng</span></a>
+				</div>
+				<div class="table-cell">
+					<a href="" class="text-center dis-table width-100" title="Nguy hại"><span class="table-cell">Nguy hại</span></a>
+					<a href="" class="text-center dis-table width-100" title="Điều trị"><span class="table-cell">Điều trị</span></a>
+				</div>
+				<div class="table-cell">
+					<a href="" class="text-center dis-table width-100" title="Chi phí"><span class="table-cell">Chi phí</span></a>
+					<a href="" class="text-center dis-table width-100" title="Kiểm tra"><span class="table-cell">Kiểm tra</span></a>
+				</div>
+			</div>
+			<div class="dis-table width-100 table1">
+				<div class="table-cell text-center">
+					<div><h2 class="text-uppercase"><a href="{{ MyAPI::getUrlTermID(29) }}">Giang mai</a></h2></div>
+				</div>
+				<div class="table-cell">
+					<a href="" class="text-center dis-table width-100" title="Nguyên nhân"><span class="table-cell">Nguyên nhân</span></a>
+					<a href="" class="text-center dis-table width-100" title="Triệu chứng"><span class="table-cell">Triệu chứng</span></a>
+				</div>
+				<div class="table-cell">
+					<a href="" class="text-center dis-table width-100" title="Nguy hại"><span class="table-cell">Nguy hại</span></a>
+					<a href="" class="text-center dis-table width-100" title="Điều trị"><span class="table-cell">Điều trị</span></a>
+				</div>
+				<div class="table-cell">
+					<a href="" class="text-center dis-table width-100" title="Chi phí"><span class="table-cell">Chi phí</span></a>
+					<a href="" class="text-center dis-table width-100" title="Kiểm tra"><span class="table-cell">Kiểm tra</span></a>
+				</div>
+			</div>
+			<div class="dis-table width-100 table1">
+				<div class="table-cell text-center">
+					<div><h2 class="text-uppercase"><a href="{{ MyAPI::getUrlTermID(31) }}">Mụn rộp sinh dục</a></h2></div>
+				</div>
+				<div class="table-cell">
+					<a href="" class="text-center dis-table width-100" title="Nguyên nhân"><span class="table-cell">Nguyên nhân</span></a>
+					<a href="" class="text-center dis-table width-100" title="Triệu chứng"><span class="table-cell">Triệu chứng</span></a>
+				</div>
+				<div class="table-cell">
+					<a href="" class="text-center dis-table width-100" title="Nguy hại"><span class="table-cell">Nguy hại</span></a>
+					<a href="" class="text-center dis-table width-100" title="Điều trị"><span class="table-cell">Điều trị</span></a>
+				</div>
+				<div class="table-cell">
+					<a href="" class="text-center dis-table width-100" title="Chi phí"><span class="table-cell">Chi phí</span></a>
+					<a href="" class="text-center dis-table width-100" title="Kiểm tra"><span class="table-cell">Kiểm tra</span></a>
+				</div>
+			</div>
+		</div>
+		<div class="row7">
+			<div class="dis-table width-100 table1">
+				<div class="table-cell text-center">
+					Hỏi đúng bệnh
+				</div>
+				<div class="table-cell">
+					<a href="http://swt.phongkham193.com/LR/Chatpre.aspx?id=MFI63108226&cid=1489654963660812714370&lng=en" class="dis-table text-center bg1 width-100" title="Niệu đạo chảy mủ">
+						<span class="table-cell">Niệu đạo chảy mủ</span>
+					</a>
+					<a href="http://swt.phongkham193.com/LR/Chatpre.aspx?id=MFI63108226&cid=1489654963660812714370&lng=en" class="dis-table text-center bg2 width-100" title="Lỗ niệu đạo sưng đau">
+						<span class="table-cell">Lỗ niệu đạo sưng đau</span>
+					</a>
+					<a href="http://swt.phongkham193.com/LR/Chatpre.aspx?id=MFI63108226&cid=1489654963660812714370&lng=en" class="dis-table text-center bg3 width-100" title="Lỗ niệu đạo ngứa rát">
+						<span class="table-cell">Lỗ niệu đạo ngứa rát</span>
+					</a>
+				</div>
+				<div class="table-cell">
+					<a href="http://swt.phongkham193.com/LR/Chatpre.aspx?id=MFI63108226&cid=1489654963660812714370&lng=en" class="dis-table text-center bg4 width-100" title="Bao quy sưng đỏ">
+						<span class="table-cell">Bao quy sưng đỏ</span>
+					</a>
+					<a href="http://swt.phongkham193.com/LR/Chatpre.aspx?id=MFI63108226&cid=1489654963660812714370&lng=en" class="dis-table text-center bg5 width-100" title="Dương vật mọc mụn">
+						<span class="table-cell">Dương vật mọc mụn</span>
+					</a>
+					<a href="http://swt.phongkham193.com/LR/Chatpre.aspx?id=MFI63108226&cid=1489654963660812714370&lng=en" class="dis-table text-center bg6 width-100" title="Những đám mụn màu hồng">
+						<span class="table-cell">Những đám mụn màu hồng</span>
+					</a>
+				</div>
+				<div class="table-cell">
+					<a href="http://swt.phongkham193.com/LR/Chatpre.aspx?id=MFI63108226&cid=1489654963660812714370&lng=en" class="dis-table text-center bg7 width-100" title="Quy đầu mọc nốt nhỏ màu đỏ">
+						<span class="table-cell">Quy đầu mọc nốt<br>nhỏ màu đỏ</span>
+					</a>
+					<a href="http://swt.phongkham193.com/LR/Chatpre.aspx?id=MFI63108226&cid=1489654963660812714370&lng=en" class="dis-table text-center bg8 width-100" title="Bao quy lở loét">
+						<span class="table-cell">Bao quy lở loét</span>
+					</a>
+				</div>
+			</div>
+		</div>
+		<div class="row8">
+			<div class="dis-table width-100">
+				<div class="table-cell cell1">
+					Nam khang
+				</div>
+				<div class="table-cell cell2">
+					Nam khoa chuyên nghiệp
+				</div>
+				<div class="table-cell cell3">
+					<a href="http://swt.phongkham193.com/LR/Chatpre.aspx?id=MFI63108226&cid=1489654963660812714370&lng=en" title="Xem thêm">>> Xem thêm</a>
+				</div>
+			</div>
+		</div>
+		<div class="row9">
+			<div class="overfllow">
+				<div class="slick-pk">
+					<div class="col1">
+						<img src="{{ asset('public/images/mobile/home-5.png') }}" alt="Hình ảnh phòng khám" class="img-responsive">
+					</div>
+					<div class="col1">
+						<img src="{{ asset('public/images/mobile/home-6.png') }}" alt="Hình ảnh phòng khám" class="img-responsive">
+					</div>
+					<div class="col1">
+						<img src="{{ asset('public/images/mobile/home-7.png') }}" alt="Hình ảnh phòng khám" class="img-responsive">
+					</div>
+					<div class="col1">
+						<img src="{{ asset('public/images/mobile/home-8.png') }}" alt="Hình ảnh phòng khám" class="img-responsive">
+					</div>
+					<div class="col1">
+						<img src="{{ asset('public/images/mobile/home-9.png') }}" alt="Hình ảnh phòng khám" class="img-responsive">
+					</div>
+					<div class="col1">
+						<img src="{{ asset('public/images/mobile/home-10.png') }}" alt="Hình ảnh phòng khám" class="img-responsive">
+					</div>
+				</div>
+			</div>
+			<script>
+				$('.slick-pk').slick({
+				  	slidesToShow: 3,
+				  	slidesToScroll: 1,
+				  	autoplay: true,
+				  	autoplaySpeed: 5000,
+				});	
+			</script>
+		</div>
+	</main>
+
+@endsection('content')
+@section('toolbar')
+	<div class="container footer-toolbar">
+		<div class="dis-table width-100">
+			<div class="table-cell cell1">
+				<div class="dis-table width-100">
+					<div class="table-cell">
+						<h3 class="text-center"><a href="tel:18006181"><i class="bg bg1"></i></a></h3>
+					</div>
+					<div class="table-cell">
+						<h4 class="text-center"><a href="tel:18006181">Điện thoại tư vấn</a></h4>
+					</div>
+				</div>
+			</div>
+			<div class="table-cell cell2">
+				<div class="dis-table width-100">
+					<div class="table-cell">
+						<h3 class="text-center"><a href="http://swt.phongkham193.com/LR/Chatpre.aspx?id=MFI63108226&cid=1489654963660812714370&lng=en"><i class="bg bg2"></i></a></h3>
+					</div>
+					<div class="table-cell">
+						<h4 class="text-center"><a href="http://swt.phongkham193.com/LR/Chatpre.aspx?id=MFI63108226&cid=1489654963660812714370&lng=en">Tư vấn trực tuyến</a></h4>
+					</div>
+				</div>
+			</div>
+			<div class="table-cell cell1">
+				<div class="dis-table width-100">
+					<div class="table-cell">
+						<h3 class="text-center"><a href="#top"><i class="bg bg3"></i></a></h3>
+					</div>
+					<div class="table-cell">
+						<h4 class="text-center"><a href="{{ url('/') }}">Quay lại</a></h4>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+@endsection('toolbar')
